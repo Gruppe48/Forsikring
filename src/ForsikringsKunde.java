b0a9398ae34c138fa6572af26a42d66305140341
Tommy Nyrud <dobbelmoral@hotmail.com>
2011-11-14 16:30:40 +0100

    Masse

--- src/ForsikringsKunde.java
+++ src/ForsikringsKunde.java
@@ -24,6 +24,7 @@ public class ForsikringsKunde
   public ForsikringsKunde(String navn, String fakturaAdresse) {
     this.navn = navn;
     this.fakturaAdresse = fakturaAdresse;
+    forsikringer = new Forsikring[MAX];
     forsikringsNr = nesteNr++; 
   }
 
@@ -36,31 +37,80 @@ public class ForsikringsKunde
     return false;
   }
   
+  public String getNavn() {
+    return navn;
+  }
+  
+  public int getForsikringsNr() {
+    return forsikringsNr;
+  }
+  
   public String tegnForsikring(Forsikring f)
   {
+    if (!erForsikret(f)) {
+      for(int i=0; i < forsikringer.length; i++) {
+        if(forsikringer[i] == null) {
+          forsikringer[i] = f;
+          
+          return "Ny forsikring er registert";
+        }
+      }
+    }
+    return "Forsikring ble ikke registert";
+  }
+    
     /*< Metoden skal opprette en ny forsikring på kunden, under forutsetning
       av at kunden ikke allerede har denne typen forsikring fra før.
       Returverdien skal inneholde en tekst med informasjon om utfallet av
       opprettelsen. > */
-  }
+  
 
   public boolean totalkunde()
   {
+    for (int i=0; i < forsikringer.length; i++) {
+      if (forsikringer[i] == null) {
+        return false;
+      }
+    }
+    return true;
+  }
+
     /*< Hvis en kunde har opprettet alle tre forsikringstyper, er kunden
       "totalkunde", og metoden skal returnere true. I motsatt fall skal
       metoden returnere false. > */
-  }
+  
 
   public double premie()
   {
+    double premie = 0;
+    
+    for(int i=0; i < forsikringer.length; i++) {
+      if(forsikringer[i] != null) {
+        premie += forsikringer[i].premie();
+      }
+    }
+    if(totalkunde()) {
+      premie = premie - ((premie/100) * Forsikring.TOTALKUNDERABATT);
+    }
+    return premie;
     /*< Metoden skal beregne, og returnere, det kunden skal betale for alle
       forsikringene til sammen. Hvis kunden er "totalkunde", dvs har både
       hus/innbo- , bil- og reiseforsikring, skal vedkommende få
       TOTALKUNDERABATT (Hint: Se klassen Forsikring). > */
   }
 
-  public String toString()
-  {
+  public String toString() {
+    String utskrift = "";
+    NumberFormat kroneformat = NumberFormat.getCurrencyInstance();
+    
+    if(totalkunde()) {
+      utskrift += "Forsikringstager er totalkunde i TRYGG FORSIKRING\n "
+              + "og får dermed" + Forsikring.TOTALKUNDERABATT + "%\n\n";
+    }
+    
+    utskrift += "SUM FORSIKRINGSPREMIER PR ÅR: kr " + kroneformat.format(premie() + "\n");
+    
+    return utskrift;
     /*< Metoden skal returnere en tekst som inneholder alle data som er lagret
       om kunden og vedkommendes forsikringer. Det skal skrives ut hva hver
       enkelt forsikring koster, og det årlige betløpet kunden må betale.
